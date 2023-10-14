eeb3211c21999cad20712613fc4a8c467360d194
willtjack <142678281+willtjack@users.noreply.github.com>
2023-10-14 14:57:33 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const items = document.querySelectorAll('.item:not(:first-child)');
+
+const options = {
+  threshold: 0.5
+}
+
+function addSlideIn(entries) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('slide-in');
+    }
+  });
+}
+
+const observer = new IntersectionObserver(addSlideIn, options)
+
+items.forEach(item => {
+  observer.observe(item);
+})
+
+const body = document.querySelector('body');
+const modeToggle = document.getElementById('mode-toggle');
+const modeStatus = document.querySelector('.mode-status');
+
+function toggleMode() {
+  body.classList.toggle('dark-mode');
+
+  const modeMessage = body.classList.contains('dark-mode')
+    ? 'Light Mode'
+    : 'Dark Mode';
+
+  modeStatus.innerText = 'Switch to ' + modeMessage;
+}
+
+modeToggle.addEventListener('click', toggleMode);
